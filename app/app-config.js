f8683ea0a2da067d8474e38fd155665e3ed84b3e
钟强 <qiang.zhong@dianping.com>
2015-04-23 15:15:22 +0800

    layout

--- app/app-config.js
+++ app/app-config.js
@@ -4,80 +4,79 @@ module.exports = {
         '/handbook': '/handbook/index',
         '/example': '/example/index'
     },
-
     navs: [
-        {
-            text: '简介',
-            path: '/handbook/instruction',
-            external: false
-        },
-        {
-            text: '环境搭建',
-            path: '/handbook/start/index',
-            external: false
-        },
-        {
-            text: '组件使用',
-            path: '/handbook/component/index',
-            children:[
-                {
-                    text: '基本介绍',
-                    path: '/handbook/component/index',
-                    external: false
-                },
-                {
-                    text: 'mode结构',
-                    path: '/handbook/component/mode',
-                    external: false
-                },
-                {
-                    text: '文本',
-                    path: '/handbook/component/text',
-                    external: false
-                },
-                {
-                    text: 'formGroup',
-                    path: '/handbook/component/form-group',
-                    external: false
-                },
-                {
-                    text: '文本输入框',
-                    path: '/handbook/component/text-box',
-                    external: false
-                },
-                {
-                    text: 'validator',
-                    path: '/handbook/service/validator',
-                    external: false
-                },
-                {
-                    text: 'notification',
-                    path: '/handbook/service/notification',
-                    external: false
-                },
-                {
-                    text: '弹出框',
-                    path: '/handbook/service/modal',
-                    external: false
-                },
-                {
-                    text: '复选组',
-                    path: '/handbook/component/checkbox-group',
-                    external: false
-                }
-            ],
-            external: false
-        },
-        {
-            text: '组件开发',
-            path: '/handbook/cdev/index',
-            external: false
-        },
-        {
-            text: '前端框架',
-            path: '/handbook/cdev/index',
-            external: false
-        }
+            {
+                text: '简介',
+                path: '/handbook/instruction',
+                external: false
+            },
+            {
+                text: '环境搭建',
+                path: '/handbook/start/index',
+                external: false
+            },
+            {
+                text: '组件使用',
+                path: '/handbook/component/index',
+                children:[
+                    {
+                        text: '基本介绍',
+                        path: '/handbook/component/index',
+                        external: false
+                    },
+                    {
+                        text: 'mode结构',
+                        path: '/handbook/component/mode',
+                        external: false
+                    },
+                    {
+                        text: '文本',
+                        path: '/handbook/component/text',
+                        external: false
+                    },
+                    {
+                        text: 'formGroup',
+                        path: '/handbook/component/form-group',
+                        external: false
+                    },
+                    {
+                        text: '文本输入框',
+                        path: '/handbook/component/text-box',
+                        external: false
+                    },
+                    {
+                        text: 'validator',
+                        path: '/handbook/service/validator',
+                        external: false
+                    },
+                    {
+                        text: 'notification',
+                        path: '/handbook/service/notification',
+                        external: false
+                    },
+                    {
+                        text: '弹出框',
+                        path: '/handbook/service/modal',
+                        external: false
+                    },
+                    {
+                        text: '复选组',
+                        path: '/handbook/component/checkbox-group',
+                        external: false
+                    }
+                ],
+                external: false
+            },
+            {
+                text: '组件开发',
+                path: '/handbook/cdev/index',
+                external: false
+            },
+            {
+                text: '前端框架',
+                path: '/handbook/cdev/index',
+                external: false
+            }
 
-    ]
+        ]
 }
